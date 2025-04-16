7c86cab3261419ab24605ca3dc855271be175eac
felipe.barraza.vega <felipe.barraza.vega@gmail.com>
2025-04-16 15:25:42 +0000

    Modificaciones objetivo 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,11 @@ import PozoDigital from './components/PozoDigital'
 import './App.css'
 
 function App() {
-  const [nivel, setNivel] = useState(4.2) // Nivel en metros
+  const [nivel, setNivel] = useState(0)
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <PozoDigital nivel={nivel} />
-
-      {/* Botones para probar subir o bajar el nivel */}
-      <div className="mt-4 space-x-4">
-        <button onClick={() => setNivel(n => n + 0.5)} className="px-4 py-2 bg-green-500 text-white rounded">Subir</button>
-        <button onClick={() => setNivel(n => n - 0.5)} className="px-4 py-2 bg-red-500 text-white rounded">Bajar</button>
-      </div>
     </div>
   )
 }
