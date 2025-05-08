66b493a2785ba5bb7416df735d71c3e87323203c
ni.jaque.a <ni.jaque.a@gmail.com>
2025-05-08 14:05:55 +0000

    modificaciones

--- src/components/PozoDigital.jsx
+++ src/components/PozoDigital.jsx
@@ -7,9 +7,9 @@ function PozoDigital({ nivel }) {
   const porcentaje = Math.max(0, Math.min(nivel / 10 * 100, 100));
 
   return (
-    <div className="relative w-64 h-96 border-4 border-brown-800 overflow-hidden rounded-xl shadow-lg bg-white">
+    <div className="relative w-64 h-96 border-4 border-brown-800 overflow-visible rounded-xl shadow-lg bg-white">
       {/* Tierra */}
-      <div className="absolute top-0 w-full h-1/2 bg-[url('/src/assets/images/textura_tierra2.jpg')] bg-cover" />
+      <div className="absolute top-0 w-full h-1/2 bg-[url('/src/assets/images/terreno1.png')] bg-cover" />
 
       {/* Agua */}
       <motion.div
@@ -25,29 +25,34 @@ function PozoDigital({ nivel }) {
 
       {/* Datalogger con tooltip embebido */}
       <div className="absolute top-1/3 left-2/3 transform -translate-x-1/2 -translate-y-1/2 group">
+        {/* Elemento que representa el Datalogger */}
         <div className="w-6 h-6 bg-black rounded-sm"></div>
+        {/* Tooltip para el Datalogger */}
         <div className="absolute -top-8 left-1/2 -translate-x-1/2 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300">
           Datalogger: recoge y transmite datos
         </div>
         {/* Imagen de DGA */}
-        <div className="absolute top-[120%] left-1/2 -translate-x-1/2">
+        <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2">
           <img src={DGALogo} alt="Logo DGA" width="30" height="30" />
         </div>
 
       </div>
 
-      {/* Caudalímetro con tooltip embebido */}
-      <div className="absolute top-[42%] left-2 group">
-        <div className="w-6 h-6 bg-gray-500 rounded-full"></div>
-        <div className="absolute -top-8 left-1/2 -translate-x-1/2 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition duration-300">
-          Caudalímetro: mide el flujo de agua
-        </div>
-        {/* Imagen de IKOLU */}
-        <div className="absolute top-[120%] left-1/2 -translate-x-1/2">
-          <img src={IKOLULogo} alt="Logo IKOLU" width="30" height="30" />
-        </div>
-      </div>
+      <div className="absolute top-[42%] left-[10%] z-50">
+  {/* Caudalímetro (icono negro) */}
+  <div className="w-6 h-6 bg-black rounded-sm"></div>
+{/* Tooltip SIEMPRE visible (sin hover) */}
+<div className="mt-2 bg-black text-white text-xs px-2 py-1 rounded text-center shadow-lg w-40">
+    <div className="font-bold">Caudalímetro</div>
+    <div>Dispositivo que mide el caudal del agua</div>
+  </div>
+  </div>
+
+      {/* Imagen de IKOLU (fuera del grupo para no interferir con el tooltip) */}
+<div className="absolute top-[50%] left-[1%] z-10">
+  <img src={IKOLULogo} alt="Logo IKOLU" width="30" height="30" />
     </div>
+  </div>
   );
 }
 
